2af14d396bf6407f4d65dd61c0dc6e2d3890e226
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-23 14:57:55 +0200

    Initial.

---
+++ Controls/ResourceKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace Controls
+{
+    using System.Windows;
+
+    public static class ResourceKeys
+    {
+        public static readonly ComponentResourceKey BrushKey = new ComponentResourceKey(
+            typeof(ResourceKeys),
+            $"{nameof(ResourceKeys)}.{nameof(BrushKey)}");
+    }
+}
